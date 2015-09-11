fc3e5c06b5c864ebc67222f48c0451441917a3b7
Louise Rains <louise.rains@meyouhealth.com>
2015-09-11 16:02:30 -0500

    add edit, update actions with form

--- app/controllers/fabrics_controller.rb
+++ app/controllers/fabrics_controller.rb
@@ -6,4 +6,15 @@ class FabricsController < ApplicationController
   def show
     @fabric = Fabric.find(params[:id])
   end
+
+  def edit
+    @fabric = Fabric.find(params[:id])
+  end
+
+  def update
+    @fabric = Fabric.find(params[:id])
+    fabric_params = params.require(:fabric).permit(:fabric_type, :color, :pattern, :length, :width, :price, :description)
+    @fabric.update(fabric_params)
+    redirect_to @fabric
+  end
 end
---
+++ spec/features/edit_fabrics_spec.rb
@@ -0,0 +1,23 @@
+describe "Editing a fabric" do
+
+  it "updates the fabric and shows the fabric's updated details" do
+    fabric = Fabric.create(fabric_attributes)
+
+    visit fabric_url(fabric)
+
+    click_link 'Edit'
+
+    expect(current_path).to eq(edit_fabric_path(fabric))
+
+    expect(find_field("Fabric type").value).to eq(fabric.fabric_type)
+    expect(find_field("Color").value).to eq(fabric.color)
+    expect(find_field("Pattern").value).to eq(fabric.pattern)
+
+    fill_in "Color", with: "chartreuse"
+
+    click_button "Update Fabric"
+
+    expect(current_path).to eq(fabric_path(fabric))
+    expect(page).to have_text("Chartreuse")
+  end
+end
